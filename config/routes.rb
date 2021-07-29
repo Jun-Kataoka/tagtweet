7f98ca1e2419c9c6ea84d40ff402cad595932a9f
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-29 18:33:03 +0900

    検索機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
-  resources :tweets, only: [:new, :create]
- end
+  resources :tweets, only: [:n
+    resources :tweets, only: [:index, :new, :create] do
+      collection do
+        get 'search'
+      end
+    end
+end
